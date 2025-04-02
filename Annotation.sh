988d944c259cc7700b7c002a1639fc2f3594ed14
khaoulaelm <136265673+khaoulaelm@users.noreply.github.com>
2025-04-02 13:43:53 +0200

    Delete Annotation.sh

--- Annotation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p prokka_results
-
-for file in ~/Desktop/article1/genomes/*.fasta; do
-    base=$(basename "$file" .fasta)
-    prokka --outdir prokka_results/$base --prefix $base --cpus 4 "$file"
-done
